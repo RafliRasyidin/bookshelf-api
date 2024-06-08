7bd76f92b0a7be831f4416d260221a1dab32a655
raflirasyidin <raflirasyidin20@gmail.com>
2024-06-08 11:04:40 +0700

    finishing get detail book

--- src/handler.js
+++ src/handler.js
@@ -122,7 +122,30 @@ const getBooksHandler = (request, h) => {
     return response
 }
 
+const getDetailBookHandler = (request, h) => {
+    const { bookId } = request.params
+    const book = books.filter(book => book.id == bookId)[0]
+    if (book === undefined) {
+        const response = h.response({
+            status: 'fail',
+            message: 'Buku tidak ditemukan'
+        })
+        response.code(404)
+        return response
+    }
+
+    const response = h.response({
+        status: 'success',
+        data: {
+            book: book
+        }
+    })
+    response.code(200)
+    return response
+}
+
 module.exports = {
     addBookHandler,
-    getBooksHandler
+    getBooksHandler,
+    getDetailBookHandler
 }
